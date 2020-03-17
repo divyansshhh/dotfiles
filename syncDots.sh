f4bf18381bb244f1caa81df37016dd090ed07b81
Divyansh Choudhary <divyanshchoudhary99@gmail.com>
2020-03-17 14:17:20 +0530

    pushing current setup

---
+++ syncDots.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/sh
+
+#Copying Config Files to dotfiles directory
+cp -r ~/.config/compton ~/dotfiles/
+cp -r ~/.config/dunst ~/dotfiles/
+cp -r ~/.config/i3 ~/dotfiles
+cp -r ~/.config/neofetch ~/dotfiles
+cp -r ~/.config/nvim/init.vim ~/dotfiles/nvim
+cp -r ~/.config/polybar ~/dotfiles
+cp -r ~/.config/ranger ~/dotfiles
+cp -r ~/.config/rofi ~/dotfiles
+cp -r ~/.config/termite ~/dotfiles
+cp ~/.zshrc .
+
